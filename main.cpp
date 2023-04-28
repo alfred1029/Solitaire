bbe9f56019488959bb0d827638bad3708649173f
Alfred <125745725+alfred1029@users.noreply.github.com>
2023-04-29 01:38:15 +0800

    Update main.cpp
    This is a temporary main function, only for development of game functions, will implement with ncurses.h in future

--- main.cpp
+++ main.cpp
@@ -2,14 +2,26 @@
 #include<string>
 #include"guiTemp.h"
 #include"initTable.h"
+#include"move.h"
 
 using namespace std;
 
 int main(){
+    // This is temporary main function for development of game functions
     vector<Card> deck = initRandomDeck();
     vector<vector<Card>> table = initTable(deck);
-    int move = 0;
-    int score = 0;
+    int move = 0, score = 0;
+    string command;
     printTable(table, move, score);
+    while (command != "e")
+    {
+        cin >> command;
+        if (command == "f"){
+            flipStock(table);
+            ++move;
+        }
+        printTable(table, move, score);
+    }
+    
     return 0;
 }
