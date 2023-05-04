db274845d2d6236f4c2930ecc030f0a1f35c15fc
Alfred <125745725+alfred1029@users.noreply.github.com>
2023-05-05 01:29:11 +0800

    Update checkInput.h
    added <string>

--- checkInput.h
+++ checkInput.h
@@ -4,12 +4,13 @@
 #include "card.h"
 #include "checkInput.h"
 #include <vector>
+#include <string>
 
 using namespace std;
 
 int checkValid(vector<vector<Card>> &table, vector<CardMap> &cardMap, int &column, int &row, string input);
 int findStack (vector<vector<Card>> &table, int &column, int &row);
 int findTarget (vector<vector<Card>> &table, int &column, int &row);
-int convertCard(string input);
+int convertCard(string Card);
 
 #endif
