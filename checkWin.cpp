e4fd617a6d062b1c9e77c5d37a8e68e427952951
allywun <125728160+allywun@users.noreply.github.com>
2023-05-05 13:34:45 +0800

    Update checkWin.cpp
    Signed-off-by: allywun <125728160+allywun@users.noreply.github.com>

--- checkWin.cpp
+++ checkWin.cpp
@@ -33,32 +33,33 @@ void moveAvailable(vector<vector<Card>> &table) {
   }
   
   //check if cards can be stacked
-  else: 
+  else {
   
-  //select two cards being shown from the end of each column
-  for (int i = 0; i < 9; i++) {
-    
-    //skip if the column is empty
-    if (table[i].size() > 0) {
-      continue;
-    }
-    
-    for (int j = 0; j < 9; j++) {
-      
+    //select two cards being shown from the end of each column
+    for (int i = 0; i < 9; i++) {
+
       //skip if the column is empty
-      if (table[j].size() == 0) {
+      if (table[i].size() > 0) {
         continue;
       }
-      
-      Card card1 = table[i][table[i].size() - 1];
-      Card card2 = table[j][table[j].size() - 1];
-        
-      //if card 1 and card 2 are opposite colosr and card 2's rank is one less than card 1's rank, they can be stacked
-      if (((card1.suit +1)%2 == (card2.suit)%2) && (card1.rank == card2.rank + 1)) {
-        return true;
+
+      for (int j = 0; j < 9; j++) {
+
+        //skip if the column is empty
+        if (table[j].size() == 0) {
+          continue;
+        }
+
+        Card card1 = table[i][table[i].size() - 1];
+        Card card2 = table[j][table[j].size() - 1];
+
+        //if card 1 and card 2 are opposite colosr and card 2's rank is one less than card 1's rank, they can be stacked
+        if (((card1.suit +1)%2 == (card2.suit)%2) && (card1.rank == card2.rank + 1)) {
+          return true;
+        } 
       }
     }
+    return false;
   }
-  return false;
 }
       
