3cd76a9fd309c66ee82276630d09b9ddae1bf060
allywun <125728160+allywun@users.noreply.github.com>
2023-05-06 02:14:40 +0800

    Update leaderboard.cpp
    Signed-off-by: allywun <125728160+allywun@users.noreply.github.com>

--- leaderboard.cpp
+++ leaderboard.cpp
@@ -1,39 +1,49 @@
-//to store scores
+//to generate new leaderboard
+
+#include "card.h"
+#include "leaderboard.h"
 #include <fstream>
 #include <vector>
 #include <algorithm>
-#include "card.h"
-#include "leaderboard.h"
+
 
 void leaderboard(&p) {
   
   ifstream oldScores;
   ofstream newScores;
+  
+  //create a vector to carry old and new scores to be sorted
   vector<int> temp;
   
+  //open old leaderboard
   oldScores.open("scores.txt");
   
-  //append old scores to the temp vector
+  //read all old scores from the leaderboard (read "scores.txt" line by line until its end)
   while(oldScores >> oldScore) {
+    
+    //append old scores to a temporary vector
     temp.push_back(oldScore);
+    
   }
   
-  //append the new score to the vector
+  //append the new score to the temporary vector
   temp.push_back(p.score);
   
-  //sort the scores from highest to lowest
+  //sort the scores in the temporary vector in descending order
   sort(temp.begin(), temp.end());
   
-  //if there are more than 10 score records, remove the lowest score
+  //keep top 10 scores (if there are more than 10 score records, remove the lowest score)
   if (temp.size() > 10) {
     temp.pop_back();
   }
   
+  //close the old leaderboard
   oldScores.close();
   
+  //open the new leaderboard, clearing "scores.txt"
   newScores.open("scores.txt");
   
-  //update the top scores
+  //append the top scores to the new leaderboard
   for (int i = 0; i < temp.size(); i++) {
     newScores << temp[i] << endl;
   }
