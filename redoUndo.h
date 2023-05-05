16c9dbdd4339ed6c0dfca797cff5b58e94b1c526
元始天尊 <106912603+THA114514@users.noreply.github.com>
2023-05-05 20:21:52 +0800

    redoUndo
    Signed-off-by: 元始天尊 <106912603+THA114514@users.noreply.github.com>

---
+++ redoUndo.h
@@ -0,0 +1,22 @@
+#ifndef REDOUNDO_H
+#define REDOUNDO_H
+
+#include "card.h"
+#include <vector>
+
+using namespace std;
+
+struct singleProcess{
+    vector<vector<Card>> table;
+    vector<CardMap> cardMap;
+    Ptr ptr;
+};
+
+vector<singleProcess> processes;
+
+void redo(vector<vector<Card>> &table, Ptr &p, vector<CardMap> &cardMap, vector<singleProcess> processes);
+void undo(vector<vector<Card>> &table, Ptr &p, vector<CardMap> &cardMap, vector<singleProcess> processes);
+void saveProcess(vector<vector<Card>> table, Ptr p, vector<CardMap> cardMap, vector<singleProcess> processes);
+void deleteProcess(vector<vector<Card>> table, Ptr p, vector<CardMap> cardMap, vector<singleProcess> processes);
+
+#endif
